80d57bb4cb1e75794f10ae3388a120caac1f2185
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-02-06 13:26:09 -0500

    added querySelectorAll to the skipButtons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 
 /* Build out functions*/
@@ -26,6 +26,10 @@ function updateButton() {
 
 }
 
+function skip() {
+  console.log('skipping');
+}
+
 /* Hook up the event listners*/
 // onClick, the video will begin playing
 video.addEventListener('click', togglePlay);
@@ -34,3 +38,4 @@ video.addEventListener('pause', updateButton);
 
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
