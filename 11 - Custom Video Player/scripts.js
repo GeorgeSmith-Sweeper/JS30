4501add603eb920085dc0ef798543028b02c47d5
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-02-06 14:11:04 -0500

    scrub function works

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -35,11 +35,27 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+// adjusts the progress bar depending on the completion of the video
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+
+
+
+
 /* Hook up the event listners*/
 // onClick, the video will begin playing
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 
 toggle.addEventListener('click', togglePlay);
@@ -49,3 +65,9 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 
 // range event listener
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown =true);
+progress.addEventListener('mouseup', () => mousedown =false);
