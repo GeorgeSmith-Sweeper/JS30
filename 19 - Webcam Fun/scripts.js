2ff1522dc7549554b71524c5cd4967463cc885f4
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-03-24 11:19:44 -0400

    created redEffect filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -7,7 +7,6 @@ const snap = document.querySelector('.snap');
 function getVideo() {
   navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(localMediaStream => {
-      console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
       video.play();
     })
@@ -17,7 +16,10 @@ function getVideo() {
 }
 
 
-
+/*
+  sets the width and height of the video to the heigh of the canvas element.
+  specifies the interval of the drawing on the canvas element
+*/
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -26,8 +28,35 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
   },16);
 }
 
+function takePhoto() {
+
+  // plays the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+
+  // allows the file to be downloaded with the name handsome
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+ for(let i = 0; i < pixels.length; i += 4) {
+   pixels[i + 0] = pixels.data[i + 0] + 100; // RED
+   pixels[i + 1] = pixels.data[i + 1] - 50; // GREEN
+   pixels[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
+ }
+}
 getVideo();
 
+video.addEventListener('canplay', paintToCanvas);
