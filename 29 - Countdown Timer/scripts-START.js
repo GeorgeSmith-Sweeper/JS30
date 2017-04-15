af3504efd3d457c9e27d829a745877ade691e983
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-04-14 21:09:10 -0400

    added the setInterval

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,4 +1,19 @@
+let countdown;
+
 function timer(seconds) {
-  // figures out when the timer started
+  // figures out when the timer started,
+  // uses static 'now' Method
   const now = Date.now()
+  const then = now + seconds * 1000;
+  //console.log({now, then});
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop the interval
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    console.log(secondsLeft)
+  }, 1000)
 }
