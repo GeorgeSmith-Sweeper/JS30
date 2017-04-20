446a00e8b5e4ef01240cac8b8f774c1881c225e7
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-04-19 21:45:02 -0400

    added endTime

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,5 +1,6 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('display__end-time');
 
 function timer(seconds) {
   // figures out when the timer started,
@@ -7,6 +8,7 @@ function timer(seconds) {
   const now = Date.now()
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then)
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -28,7 +30,14 @@ function displayTimeLeft(seconds) {
   const remainderSeconds = seconds % 60;
   // ternary adds a zero as padding to the remaining time
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
   // displays the time
   timerDisplay.textContent = display;
 }
 
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back at ${hour}:${minutes}`;
+}
